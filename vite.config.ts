ad7b14c733b0ae9683953b030bd7661bd9e0cb3d
Isurinda D. Perera <159692818+Yuraj-Isurinda@users.noreply.github.com>
2025-06-29 18:20:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/yurajIsurinda/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
